d36e56126cab071c30060c2b6cfe445e49ab7aaa
abrari <muhammad.abrari@gmail.com>
2014-04-02 16:56:16 +0700

    Src test

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Created by abrari on 4/2/14.
+ */
+
+import java.util.List;
+
+public class Test {
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello IntelliJ");
+    }
+
+}
