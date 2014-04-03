0c70ef99e6eea5992084cef5b88d8cf772627e20
abrari <muhammad.abrari@gmail.com>
2014-04-03 15:50:47 +0700

    Samtools API test

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,40 @@
 /**
+ *
+ * Testing samtools SDK
+ *
  * Created by abrari on 4/2/14.
  */
 
-import java.util.List;
+import net.sf.samtools.*;
+
+import java.io.File;
 
 public class Test {
 
     public static void main(String args[])
     {
-        System.out.println("Hello IntelliJ");
+        File inputFile = new File("/home/abrari/Tesis/sam/output.bam");
+
+        SAMFileReader input = new SAMFileReader(inputFile, new File(inputFile.getAbsolutePath() + ".bai"));
+        AbstractBAMFileIndex bamIndex = (AbstractBAMFileIndex) input.getIndex();
+
+        int recordCount = 0;
+        int mappedRecordCount = 0;
+
+        for (SAMRecord samRecord : input)
+        {
+            recordCount++;
+        }
+
+        System.out.println("Number of records: " + recordCount);
+
+        for (int i = 0; i < bamIndex.getNumberOfReferences(); i++)
+        {
+            mappedRecordCount += bamIndex.getMetaData(i).getAlignedRecordCount();
+        }
+
+        System.out.println("Number of mapped records: " + mappedRecordCount);
+
     }
 
 }
