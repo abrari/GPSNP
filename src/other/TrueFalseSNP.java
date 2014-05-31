1b0046082f1c00b334f2ebadeb539da37be13753
abrari <muhammad.abrari@gmail.com>
2014-05-31 11:31:35 +0700

    Print true/false statistics for all distinct contigs

--- src/other/TrueFalseSNP.java
+++ src/other/TrueFalseSNP.java
@@ -2,6 +2,7 @@ package other;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -65,27 +66,51 @@ public class TrueFalseSNP {
         BufferedReader reader = new BufferedReader(new FileReader(candidateSnpPath));
         String line;
 
-        int trueCount = 0;
-        int falseCount = 0;
+        // Key: contig name, value = [0: trueCount, 1: falseCount]
+        HashMap<String, int[]> contigTFCount = new HashMap<String, int[]>();
 
         while((line = reader.readLine()) != null) {
 
             String[] d = line.split("\t");
+            if(d[0].equalsIgnoreCase("Chr")) continue;
+
             System.out.print(d[0] + ":" + d[1] + "\t");
 
             if(candidateIsTrue(d[0], Integer.valueOf(d[1]))) {
-                trueCount++;
                 System.out.print("✓");
+
+                int[] tf = contigTFCount.get(d[0]);
+                if(tf == null) {
+                    tf = new int[2];
+                    tf[0]++;
+                    contigTFCount.put(d[0], tf);
+                } else {
+                    tf[0]++;
+                }
             } else {
-                falseCount++;
+                int[] tf = contigTFCount.get(d[0]);
+                if(tf == null) {
+                    tf = new int[2];
+                    tf[1]++;
+                    contigTFCount.put(d[0], tf);
+                } else {
+                    tf[1]++;
+                }
             }
 
             System.out.println();
 
         }
 
-        System.err.println("True variants:  " + trueCount + " (" + (double)trueCount/(trueCount+falseCount)*100 + "%)");
-        System.err.println("False variants: " + falseCount + " (" + (double)falseCount/(trueCount+falseCount)*100 + "%)");
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        for (Map.Entry<String, int[]> entry : contigTFCount.entrySet()) {
+            int trueCount = entry.getValue()[0];
+            int falseCount = entry.getValue()[1];
+            System.err.println("Statistics for " + entry.getKey() + ":");
+            System.err.println("- True variants:  " + trueCount + " (" +  df.format((double)trueCount/(trueCount+falseCount)*100) + "%)");
+            System.err.println("- False variants: " + falseCount + " (" + df.format((double)falseCount/(trueCount+falseCount)*100) + "%)");
+        }
 
     }
 
